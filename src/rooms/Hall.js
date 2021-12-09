e14c497d954960b28cebec7848230282be9af6e4
Anna Leach <anna.leach@guardian.co.uk>
2021-12-09 20:43:23 +0000

    hi

--- src/rooms/Hall.js
+++ src/rooms/Hall.js
@@ -10,8 +10,6 @@ import soundtwo from "../assets/sounds/hall-mailbox-full.mp3"
 import soundone from "../assets/sounds/hall-mailbox-not-setup.mp3"
 import soundintro from "../assets/sounds/hall-intro-where.mp3"
 
-
-
 const introText = "Gooooooood afternoon, Mrs. Galavaten! Hope you’re ready for a long walk… Hello? Mrs. Galavaten? Where are you…?"
 const textCallOne = "Call Ms Galavaten.... okay she has probably not even switched that thing on"
 const textCallTwo = "Call Ms Galavaten's son...  that useless guy never picks up"
