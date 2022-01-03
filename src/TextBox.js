5495416b5f9b7b6dc613a64608c9245db61a9a23
Anna Leach <anna.leach@guardian.co.uk>
2022-01-03 20:36:57 +0000

    Text: make easier to edit

--- src/TextBox.js
+++ src/TextBox.js
@@ -8,7 +8,8 @@ function TextBox({ text, visible, setTextVisible }) {
   return (
         <div className={visible ? "text-box show" : "text-box fade"} onClick={() => setTextVisible(false)}>
             <div className="text-holder">
-                <p>{text}</p>
+                <p dangerouslySetInnerHTML={{__html: text}}></p>
+
             </div>
         </div>
   );
--- src/allText.js
+++ src/allText.js
@@ -1,35 +1,38 @@
 // all text for all scenes 
 
 // intro scene 
-export const textOne = "Margate, England. June 21, 2021. 4:55PM: You arrive early for your shift."
+export const textOne = `<strong>Margate, England.</strong> <br/> June 21, 2021. 4:55PM: You arrive early for your shift.`
 
 // hall 
-export const introText = "Gooooooood afternoon, Mrs. Galavaten! Hope you’re ready for a long walk… Hello? Mrs. Galavaten? Where are you…?"
+export const introText = `Gooooooood afternoon, Mrs. Galavaten! Hope you’re ready for a long walk… Hello? Mrs. Galavaten? Where are you…?`
 
 // bedroom 
-export const bmTextCellphone = "Hmm I wonder why Mrs. Galavaten didn’t take her phone. Hmm. Oh my it appears to be locked"
-export const bmTextShoes = "Oh no! Mrs. Galavaten’s favourite shoes. She never leaves them behind. She nevers leaves the house without them"
-export const bmTextClothes = "Mrs. Galavaten had a larger than life personality – and her wardrobe matches her Liberace style! But even these clothes look like they are for a special occasion…"
-export const bmTextSwitch = "Hmm strange. The lights are turned off. Mrs. Galavaten never turned the lights off in her bedroom."
-export const bmTextChopsticks = "Mrs. Galavaten was a hoarder. One of her favourite collectibles were Japanese fans and chopsticks. Travel was another thing we bonded over. Even before I moved to the UK, I loved traveling to the United States, Germany and even South Africa. Mrs. Galavaten really perked up when I showed her my photos."
-export const bmTextJewelry = "The jewelry box. It’s empty! The fish necklace that is usually there seems to be missing. It was Mrs. Galavaten’s favorite piece of jewelry. Oh no."
-export const bmTextDoll = "Oh no that creepy doll. This doll always gives me the creeps. Mrs. Galavaten got it from a friend in the Bahamas 40 years ago. I always thought it was a bit racist. Back home in Jamaica, dolls like this were only bought by white tourists. A little memento of their exotic adventure. I wish she’d let me move it…"
-export const bmTextReport = "Police report filed by Edgar Galavaten. Looks like a police report filed by Mrs. Galavaten’s older son. It’s hard to make out the text, but from what I can read: “On June 14, 2021, at 6:30pm, an altercation occurred between Edgar Galavaten and James Galavaten in their mother’s home… Alleged disagreement over money owed… large debts unpaid. Charges were dropped by Edgar Galavaten.”"
+export const bmTextCellphone = `Hmm I wonder why Mrs. Galavaten didn’t take her phone. Hmm. Oh my it appears to be locked`
+export const bmTextShoes = `Oh no! Mrs. Galavaten’s favourite shoes. She never leaves them behind. She nevers leaves the house without them`
+export const bmTextClothes = `Mrs. Galavaten had a larger than life personality – and her wardrobe matches her Liberace style! But even these clothes look like they are for a special occasion…`
+export const bmTextSwitch = `Hmm strange. The lights are turned off. Mrs. Galavaten never turned the lights off in her bedroom.`
+export const bmTextChopsticks = `Mrs. Galavaten was a hoarder. One of her favourite collectibles were Japanese fans and chopsticks. Travel was another thing we bonded over. Even before I moved 
+                                to the UK, I loved traveling to the United States, Germany and even South Africa. Mrs. Galavaten really perked up when I showed her my photos.`
+export const bmTextJewelry = `The jewelry box. It’s empty! The fish necklace that is usually there seems to be missing. It was Mrs. Galavaten’s favorite piece of jewelry. Oh no.`
+export const bmTextDoll = `Oh no that creepy doll. This doll always gives me the creeps. Mrs. Galavaten got it from a friend in the Bahamas 40 years ago. I always thought it was a bit racist. 
+                            Back home in Jamaica, dolls like this were only bought by white tourists. A little memento of their exotic adventure. I wish she’d let me move it…`
+export const bmTextReport = `Police report filed by Edgar Galavaten. Looks like a police report filed by Mrs. Galavaten’s older son. It’s hard to make out the text, but from what I can read: 
+                            “On June 14, 2021, at 6:30pm, an altercation occurred between Edgar Galavaten and James Galavaten in their mother’s home… Alleged disagreement over money owed… large debts unpaid. Charges were dropped by Edgar Galavaten.”`
  
 // kitchen 
-export const ktTextCash = "The cash register looks empty. This is where Mrs. Galavaten keeps her cash. She gives it to her grandkids and great grandkids whenever they visit, which is seldom these days."
-export const ktTextPhone = "The landline phone with the notepad next to it. It looks like Mrs. Galavaten’s handwriting. The scribbled note reads: 'Michael Dayton called from HSBC. Suspicious activity on the account. Call Edgar to find out what happened'."
-export const ktTextObit = "Maria Perez, 68, of Margate died on Wednesday, 9 June 2004 in Queen Elizabeth The Queen Mother Hospital. Born in Margate on May 26, 1936, she was the daughter of the late Alonzo Perez, Jr. and Rhoda Mae Perez. Never married, she was an avid traveler and loved her dog Skip. She is survived by her two sisters."
+export const ktTextCash = `The cash register looks empty. This is where Mrs. Galavaten keeps her cash. She gives it to her grandkids and great grandkids whenever they visit, which is seldom these days.`
+export const ktTextPhone = `The landline phone with the notepad next to it. It looks like Mrs. Galavaten’s handwriting. The scribbled note reads: 'Michael Dayton called from HSBC. Suspicious activity on the account. Call Edgar to find out what happened'.`
+export const ktTextObit = `Maria Perez, 68, of Margate died on Wednesday, 9 June 2004 in Queen Elizabeth The Queen Mother Hospital. Born in Margate on May 26, 1936, she was the daughter of the late Alonzo Perez, Jr. and Rhoda Mae Perez. Never married, she was an avid traveler and loved her dog Skip. She is survived by her two sisters.`
 
 // living 
-export const livTextCertificate = "Her certificate of achievement. Let me read this one: “Margate’s Local Hero Award 2015. 30 Years of Dedication and Service to the Community.” Even though we were boss and employee, activism was one of the few ways Mrs. Galavaten and I connected as friends. Yeah, she was an activist. I’m an activist. I love that."
-export const livTextScarf = "Hmm her scarf. Mrs. Galavaten takes this scarf everywhere with her. In fact, she never leaves the house without it. It was a gift from an old colleague at her school, where she taught."
-export const livTextPortrait = "Portrait of Mr. Galavaten. Mr. Galavaten was an avid fisherman. He passed away in 2017 – just a year before I started caring for Mrs. Galavaten. By then, she had already been diagnosed with dementia. The two got married in 1954 – when she was only 18 years-old. I heard from one care worker that it was a marriage of convenience. Another told me Mr. Galavaten had many lovers on the side. "
+export const livTextCertificate = `Her certificate of achievement. Let me read this one: “Margate’s Local Hero Award 2015. 30 Years of Dedication and Service to the Community.” Even though we were boss and employee, activism was one of the few ways Mrs. Galavaten and I connected as friends. Yeah, she was an activist. I’m an activist. I love that.`
+export const livTextScarf = `Hmm her scarf. Mrs. Galavaten takes this scarf everywhere with her. In fact, she never leaves the house without it. It was a gift from an old colleague at her school, where she taught.`
+export const livTextPortrait = `Portrait of Mr. Galavaten. Mr. Galavaten was an avid fisherman. He passed away in 2017 – just a year before I started caring for Mrs. Galavaten. By then, she had already been diagnosed with dementia. The two got married in 1954 – when she was only 18 years-old. I heard from one care worker that it was a marriage of convenience. Another told me Mr. Galavaten had many lovers on the side. `
 
 
 // driveway 
-export const driveTextCar = "Ah the Lexus. Mrs. Galavaten’s prized car. She used to cruise around the Margate in the late 90’s just before the seaside town started to decline. Decades of austerity and Thatcherism meant that everything went into disrepair, including the Dreamland amusement park."
-export const driveTextCarDoor = "Oh the Lexus. Oh the door appears to be locked."
+export const driveTextCar = `Ah the Lexus. Mrs. Galavaten’s prized car. She used to cruise around the Margate in the late 90’s just before the seaside town started to decline. Decades of austerity and Thatcherism meant that everything went into disrepair, including the Dreamland amusement park.`
+export const driveTextCarDoor = `Oh the Lexus. Oh the door appears to be locked.`
 export const driveTextPostcard = `
             Oh let me see. Oh there are some postcards. Tons of them addressed to Mrs. Galavaten. The name of the sender appears illegible. It reads: 
 
@@ -51,6 +54,8 @@ export const driveTextPostcard = `
 
 // waterfront 
 
-export const benchTextMrsG = "Mrs. Galavaten. I found you! Hello Mrs. Galavaten. You look beautiful today. Should we look at the waves a bit longer? I’m in the mood for it. I’m so happy I found you. I can’t believe I found you. Mrs. Galavaten."
-export const benchTextBeach = "The beach. What a beautiful waterfront. I have walked along these sands so many times with Lililove. Laughing and holding hands. Stealing kisses and catching glimpses of each other’s gaze. Our matching guayabera shirts flapping in the salty winds of Margate."
-export const benchTextPasser = "Have you seen an elderly lady? Oh… you haven’t? Thank you anyway."
+export const benchTextMrsG = `Mrs. Galavaten. I found you! Hello Mrs. Galavaten. You look beautiful today. 
+                                Should we look at the waves a bit longer? I’m in the mood for it. I’m so happy I found you. I can’t believe I found you. Mrs. Galavaten.`
+export const benchTextBeach = `The beach. What a beautiful waterfront. I have walked along these sands so many 
+                                times with Lililove. Laughing and holding hands. Stealing kisses and catching glimpses of each other’s gaze. Our matching guayabera shirts flapping in the salty winds of Margate.`
+export const benchTextPasser = `Have you seen an elderly lady? Oh… you haven’t? Thank you anyway.`
